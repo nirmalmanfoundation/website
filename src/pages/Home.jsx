24fb7bf25bd974222aa5e9bfa64b893c47a8cbeb
Mrityunjay Mishra <mrityunjay281203@gmail.com>
2025-03-13 17:55:13 +0530

    fixed responsive issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Banner from "../components/Banner";
-// import MissionSection from "../components/MissionSection";
 import Hero from "../components/Hero";
+
 const Home = () => {
   return (
-    <div>
-      <Banner />
-      <Hero />
-      {/* <MissionSection /> */}
+    <div className="w-full min-h-screen flex flex-col items-center bg-gray-100">
+      <div className="w-full">
+        <Banner />
+      </div>
+
+      <div className="w-full max-w-screen-xl mx-auto px-4 md:px-8">
+        <Hero />
+      </div>
     </div>
   );
 };
